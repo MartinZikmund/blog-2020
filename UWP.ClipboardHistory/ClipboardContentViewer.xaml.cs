f3d1825c066bf31013cfaddafbc49e9391e5b8b7
Martin Zikmund <martin@zikmund.dev>
2020-09-01 11:04:44 +0200

    Clipboard history sample

---
+++ UWP.ClipboardHistory/ClipboardContentViewer.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Graphics.Imaging;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace UWP.ClipboardHistory
+{
+    public sealed partial class ClipboardContentViewer : UserControl
+    {
+        public ClipboardContentViewer()
+        {
+            this.InitializeComponent();
+        }
+
+        public DataPackageView Data
+        {
+            get { return (DataPackageView)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register(nameof(Data), typeof(DataPackageView), typeof(ClipboardContentViewer), new PropertyMetadata(null, OnDataChanged));
+
+        private static async void OnDataChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewer = (ClipboardContentViewer)d;
+            viewer.ContentGrid.Children.Clear();
+            if (e.NewValue is DataPackageView view)
+            {
+                if (view.Contains(StandardDataFormats.Bitmap))
+                {
+                    var bitmapReference = await view.GetBitmapAsync();
+                    if (bitmapReference != null)
+                    {
+                        var bitmap = new BitmapImage();
+                        await bitmap.SetSourceAsync(await bitmapReference.OpenReadAsync());
+                        viewer.ContentGrid.Children.Add(new Image() { Width = 300, Height = 300, Stretch = Stretch.None, Source = bitmap });
+                    }
+                }
+                else if (view.Contains(StandardDataFormats.Text))
+                {
+                    var text = await view.GetTextAsync();
+                    if (text != null)
+                    {
+                        viewer.ContentGrid.Children.Add(new TextBlock() { Text = text });
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UWP.ClipboardHistory/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace UWP.ClipboardHistory
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            Clipboard.HistoryChanged += Clipboard_HistoryChanged;
+            Clipboard.HistoryEnabledChanged += Clipboard_HistoryEnabledChanged;            
+        }
+
+        private async void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            // Artificial delay, as in non-debug Clipboard.GetHistoryItemsAsync returns 
+            // until the app is fully loaded.
+            await Task.Delay(100); 
+            await ReloadHistoryAsync();
+        }
+
+        public ObservableCollection<ClipboardHistoryItem> ClipboardHistory { get; } = new ObservableCollection<ClipboardHistoryItem>();
+
+        private async void Clipboard_HistoryChanged(object sender, ClipboardHistoryChangedEventArgs e)
+        {
+            await ReloadHistoryAsync();
+        }
+
+        private async Task ReloadHistoryAsync()
+        {
+            var historyItems = await Clipboard.GetHistoryItemsAsync();
+            if (historyItems.Status == ClipboardHistoryItemsResultStatus.Success)
+            {
+                ClipboardHistory.Clear();
+                foreach (var item in historyItems.Items)
+                {
+                    ClipboardHistory.Add(item);
+                }
+            }
+        }
+
+        private void Clipboard_HistoryEnabledChanged(object sender, object e)
+        {
+            var isEnabled = Clipboard.IsHistoryEnabled();
+            ClipboardHistoryDisabledMessage.Visibility = isEnabled ? Windows.UI.Xaml.Visibility.Collapsed : Windows.UI.Xaml.Visibility.Visible;
+        }
+    }
+}
